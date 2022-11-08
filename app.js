e6f3e0c18930cdfc7fee33f3430aa9f33944c4cf
Matheus Sena <msena2103@gmail.com>
2022-11-07 21:09:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
     const handlebars = require('express-handlebars')
     const bodyParser = require('body-parser')
     const app = express()
+    const admin = require('./routes/admin')
     //const mongoose = require('mongoose')
 
 // Configurações
@@ -10,15 +11,16 @@
         app.use(bodyParser.urlencoded({extended: true}))
         app.use(bodyParser.json())
     // Handlebars
-        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars')
     // Mongoose
         // Em breve
 // Rotas
+    app.use('/admin', admin)
 
 
 // Outros
 const PORT = 8081
-app,listen(PORT,() => {
+app.listen(PORT,() => {
     console.log('Servidor rodando!');
 })
