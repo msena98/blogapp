12912c0397f9155512dbf7a29539f3d9ea6f75d6
Matheus Sena <msena2103@gmail.com>
2022-11-08 22:23:21 -0300

    Update app.js (Config mongoose.connect)

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
     const app = express()
     const admin = require('./routes/admin')
     const path = require('path')
-    //const mongoose = require('mongoose')
+    const { default: mongoose } = require('mongoose')
 
 // Configurações
     // Body Parser
@@ -15,7 +15,11 @@
         app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars')
     // Mongoose
-        // Em breve
+        mongoose.connect('mongodb://0.0.0.0:27017/blogapp').then(() => {
+            console.log('Conectado ao MongoDB');
+        }).catch((err) => {
+            console.log(`Erro ao se conectar: ${err}`);
+        })
     // Public
         app.use(express.static(path.join(__dirname, 'public')))
 // Rotas
